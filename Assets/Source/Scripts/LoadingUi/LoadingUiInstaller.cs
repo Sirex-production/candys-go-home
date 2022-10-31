c5323dc3564185abf5f2bbbac9a805828c15cd9b
Vo1z <vitya.voody@gmail.com>
2022-10-31 23:21:36 +0100

    Changes in loading ui

---
+++ Assets/Source/Scripts/LoadingUi/LoadingUiInstaller.cs
@@ -0,0 +1,18 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+using Zenject;
+
+namespace Candy.LoadingUi
+{
+	public sealed class LoadingUiInstaller : MonoInstaller
+	{
+		[Required, SerializeField] private UiLoadingService uiLoadingService;
+		
+		public override void InstallBindings()
+		{
+			Container.Bind<IUiLoadingService>()
+				.FromInstance(uiLoadingService)
+				.AsSingle();
+		}
+	}
+}
