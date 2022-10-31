db97bda8cbd5346cafd95957a5697bd606aaf814
Vo1z <vitya.voody@gmail.com>
2022-10-31 22:47:58 +0100

    Loading service was added

---
+++ Assets/Source/Scripts/LoadingUi/IUiLoadingService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Candy.LoadingUi
+{
+	public interface IUiLoadingService
+	{
+		public event Action OnLoadingUiShowRequested;
+		public event Action OnLoadingUiHideRequested;
+		public event Action<float> OnLoadingProgressUiUpdated;
+	}
+}
---
+++ Assets/Source/Scripts/LoadingUi/UiLoadingService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Support;
+using UnityEngine;
+using Zenject;
+
+namespace Candy.LoadingUi
+{
+	public sealed class UiLoadingService : MonoBehaviour, IUiLoadingService
+	{
+		private LevelManagementService _levelManagementService;
+
+		public event Action OnLoadingUiShowRequested;
+		public event Action OnLoadingUiHideRequested;
+		public event Action<float> OnLoadingProgressUiUpdated;
+		
+		[Inject]
+		private void Construct(LevelManagementService levelManagementService)
+		{
+			_levelManagementService = levelManagementService;
+		}
+
+		private void Awake()
+		{
+			_levelManagementService.OnLoadingStarted += OnLoadingStarted;
+			_levelManagementService.OnLoadingFinished += OnLoadingFinished;
+			_levelManagementService.OnLoadingProgressUpdated += OnLoadingProgressUpdated;
+		}
+		
+		private void OnDestroy()
+		{
+			_levelManagementService.OnLoadingStarted -= OnLoadingStarted;
+			_levelManagementService.OnLoadingFinished -= OnLoadingFinished;
+			_levelManagementService.OnLoadingProgressUpdated -= OnLoadingProgressUpdated;
+		}
+
+		private void OnLoadingStarted()
+		{
+			OnLoadingUiShowRequested?.Invoke();
+		}
+		
+		private void OnLoadingFinished()
+		{
+			OnLoadingUiHideRequested?.Invoke();
+		}
+
+		private void OnLoadingProgressUpdated(float progress)
+		{
+			const float additionalProgressOffset = .15f;
+			OnLoadingProgressUiUpdated?.Invoke(progress + additionalProgressOffset);
+		}
+	}
+}
