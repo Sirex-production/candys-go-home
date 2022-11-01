d761f6455e2a2cd6092c3cad2385555c49e1fb8b
Vo1z <vitya.voody@gmail.com>
2022-11-01 02:44:30 +0100

    Level 1 was updated

---
+++ Assets/Source/Scripts/SFX/MusicStarter.cs
@@ -0,0 +1,27 @@
+﻿using Support.Audio;
+using UnityEngine;
+using Zenject;
+
+namespace Candy.SFX
+{
+	public sealed class MusicStarter : MonoBehaviour
+	{
+		private AudioService _audioService;
+		
+		[Inject]
+		private void Construct(AudioService audioService)
+		{
+			_audioService = audioService;
+		}
+
+		private void Start()
+		{
+			_audioService.PlayAudio("music");
+		}
+
+		private void OnDestroy()
+		{
+			_audioService.StopAll();
+		}
+	}
+}
